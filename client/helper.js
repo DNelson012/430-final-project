2e07f59ac000f8e73e121b52723d73edd3ed0c18
DNelson012 <dgn8758@rit.edu>
2023-11-19 15:47:14 -0500

    Starting code using DomoMaker E

---
+++ client/helper.js
@@ -0,0 +1,70 @@
+/* Takes in an error message. Sets the error message up in html, and
+   displays it to the user. Will be hidden by other events that could
+   end in an error.
+*/
+const handleError = (message) => {
+  document.getElementById('errorMessage').textContent = message;
+  document.getElementById('domoMessage').classList.remove('hidden');
+};
+
+const hideError = () => {
+  document.getElementById('domoMessage').classList.add('hidden');
+}
+
+/* Sends post requests to the server using fetch. Will look for various
+   entries in the response JSON object, and will handle them appropriately.
+*/
+const sendPost = async (url, data, handler) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+  document.getElementById('domoMessage').classList.add('hidden');
+
+  if(result.redirect) {
+    window.location = result.redirect;
+  }
+
+  if(result.error) {
+    handleError(result.error);
+  }
+
+  if(handler) {
+    handler(result);
+  }
+};
+
+const sendDelete = async (url, data, handler) => {
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response;
+  console.log(result);
+  document.getElementById('domoMessage').classList.add('hidden');
+
+  // if(result.error) {
+  //   handleError(result.error);
+  // }
+
+  if(handler) {
+    //handler(result);
+    handler();
+  }
+}
+
+module.exports = {
+  handleError,
+  hideError,
+  sendPost,
+  sendDelete,
+}
---
+++ server/models/Domo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  level: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  level: doc.level,
+  age: doc.age,
+});
+
+const DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports = DomoModel;
