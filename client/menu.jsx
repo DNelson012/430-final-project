ce6cee19db016a315ba6546be809f6c8eb340060
DNelson012 <dgn8758@rit.edu>
2023-11-28 18:49:10 -0500

    Added in a lobby system, at least a start

--- client/menu.jsx
+++ client/menu.jsx
@@ -4,38 +4,139 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 
+// Socket Functions
+const socket = io();
 
-const returnToMenu = () => {
+const handleCreateLobby = () => {
+  const name = document.querySelector('#displayName').value;
+  const lobbyID = document.querySelector("#lobbyInput").value;
+  // Check if there was any value given
+  if (!lobbyID || !name) { return; }
+
+  // Doing it like this, there should be a check for duplicates
+  // Anyway
+  socket.emit('create lobby', { lobbyID, name });
+
+  ReactDOM.render(<LobbyLounge lobbyID={lobbyID} />,
+    document.querySelector('#content'));
+
+
+  return;
+}
+
+const handleJoinLobby = () => {
+  const name = document.querySelector('#displayName').value;
+  const lobbyID = document.querySelector("#lobbyInput").value;
+  // Check if there was any value given
+  if (!lobbyID || !name) { return; }
+
+  socket.emit('join lobby', { lobbyID, name });
+
+  ReactDOM.render(<LobbyLounge lobbyID={lobbyID} />,
+    document.querySelector('#content'));
+
+  return;
+}
+
+const displayToChat = (text) => {
+  const chat = document.querySelector('#lobbyChat');
+  // If there is no chat, don't do anything
+  if (!chat) { return; }
+
+  const textSpan = document.createElement('span');
+  textSpan.innerText = text;
+  chat.appendChild(textSpan);
+}
+
+const leaveLobby = () => {
+  // If you were in a lobby, leave it
+  socket.emit('leave lobby');
+
+  // Render the menu
+  showMenu();
+}
+
+
+
+// React Functions
+const showMenu = () => {
   ReactDOM.render(<LobbyMenu />,
     document.querySelector('#content'));
 }
 
-const joinLobby = () => {
+const showJoinLobby = () => {
   ReactDOM.render(<LobbyJoin />,
     document.querySelector('#content'));
 }
 
-const createLobby = () => {
-  ReactDOM.render(<LobbyCreation />,
+const showCreateLobby = () => {
+  ReactDOM.render(<LobbyCreate />,
     document.querySelector('#content'));
 }
 
 
 
-const LobbyJoin = () => {
+// React Components
+const LobbyLounge = (props) => {
   return (
-    <div id='lobbyJoin'>
-      <span>Join a lobby</span>
-      <button onClick={returnToMenu}>Return</button>
+    <div id='lobbyLounge'>
+      <button
+        className='buttonSmall'
+        onClick={leaveLobby}>
+        Leave Lobby
+      </button>
+      <span>Waiting in a lobby: {props.lobbyID}</span>
+      <div id='lobbyChat'></div>
     </div>
   );
 }
 
-const LobbyCreation = () => {
+const LobbyJoin = (props) => {
+  return (
+    <div id='lobbyJoin'>
+      <button
+        className='buttonSmall'
+        onClick={showMenu}>
+        Return
+      </button>
+      <span>Join a lobby</span>
+      <label htmlFor="displayName">Enter a name</label>
+      <input type="text" name="displayName"
+        id="displayName" placeholder='Name' />
+      <label htmlFor="lobbyStr">Enter a lobby ID</label>
+      <input type="text" name="lobbyStr"
+        id="lobbyInput" placeholder='Lobby ID' />
+      <button
+        className='buttonLarge'
+        id='lobbySubmit'
+        onClick={handleJoinLobby}>
+        Join Lobby
+      </button>
+    </div>
+  );
+}
+
+const LobbyCreate = (props) => {
   return (
     <div id='lobbyCreate'>
+      <button
+        className='buttonSmall'
+        onClick={showMenu}>
+        Return
+      </button>
       <span>Making a lobby</span>
-      <button onClick={returnToMenu}>Return</button>
+      <label htmlFor="displayName">Enter a name</label>
+      <input type="text" name="displayName"
+        id="displayName" placeholder='Name' />
+      <label htmlFor="lobbyStr">Enter a lobby ID</label>
+      <input type="text" name="lobbyStr"
+        id="lobbyInput" placeholder='Lobby ID' />
+      <button
+        className='buttonLarge'
+        id='lobbySubmit'
+        onClick={handleCreateLobby}>
+        Create Lobby
+      </button>
     </div>
   );
 }
@@ -43,13 +144,16 @@ const LobbyCreation = () => {
 const LobbyMenu = (props) => {
   return (
     <div id='lobbyMenu'>
-      <button onClick={createLobby}>Create Lobby</button>
-      <button onClick={joinLobby}>Join Lobby</button>
+      <button className='buttonLarge' onClick={showCreateLobby}>Create Lobby</button>
+      <button className='buttonLarge' onClick={showJoinLobby}>Join Lobby</button>
     </div>
   );
 }
 
 const init = () => {
+  socket.on('user join', displayToChat);
+  socket.on('user leave', displayToChat);
+
   ReactDOM.render(<LobbyMenu />,
     document.querySelector('#content'));
 }
--- server/io.js
+++ server/io.js
@@ -3,25 +3,96 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 let io;
+const idToName = {};
+const idToLobby = {};
 
-const handleChatMessage = (socket, msg) => {
+// Helpers
+// https://stackoverflow.com/questions/23187013/is-there-a-better-way-to-sanitize-input-with-javascript
+/*
+const sanitizeString = (inStr) => {
+  let str = inStr
+  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
+  return str.trim();
+  // If this is taking in URLs, 
+  // I can do this on individual segments between '/' characters
+  //  And also discard anything past '?' on the last segment
+}
+*/
+
+// Leave every lobby, if the user is in any
+//  A bit of a nuclear approach, but it should work fine
+const leaveAllRooms = (socket) => {
   socket.rooms.forEach(room => {
-    if (room === socket.id) return;
-
-    io.to(room).emit('chat message', msg);
-  });
-};
-
-const handleRoomChange = (socket, roomName) => {
-  socket.rooms.forEach(room => {
-    if (room === socket.id) return;
+    if (room === socket.id) return; // Ignore the socket ID
     socket.leave(room);
   });
-  socket.join(roomName);
+}
+
+// Call a function only for members of a given lobby
+const doForLobby = (socket, func) => {
+  // If there are no rooms, it says zero
+  //  This is bad when I want that id room
+  //  So this calls it immediately if the id is the only room
+  if (socket.rooms.size === 0) { func(); }
+  socket.rooms.forEach(room => {
+    if (room === socket.id) return;
+
+    func()
+  });
+}
+
+
+
+// const updatePlayerList = (socket, data) => {
+
+// }
+
+// Join the given lobby
+const handleJoinLobby = (socket, data) => {
+  leaveAllRooms(socket);
+  socket.join(data.lobbyID);
+  idToName[socket.id] = data.name;
+  idToLobby[socket.id] = data.lobbyID;
+
+  // Tell everyone in the lobby that someone joined
+  doForLobby(socket,
+    () => {
+      io.to(data.lobbyID).emit('user join', `${data.name} joined the lobby.`);
+    });
 };
 
+// Create & Join the given lobby
+const handleCreateLobby = (socket, data) => {
+  // uhhh, create it? maybe not needed. 
+  //  definitely is, but not yet.
+
+  // Join the newly created lobby
+  handleJoinLobby(socket, data);
+};
+
+const handleLeaveLobby = (socket) => {
+  // Get the display name and lobby
+  const name = idToName[socket.id];
+  const lobby = idToLobby[socket.id];
+  delete idToName[socket.id];
+  delete idToLobby[socket.id];
+
+  // If there is no lobby, don't do anything else
+  if (!lobby) { return; }
+
+  // Tell everyone in the lobby that someone left
+  doForLobby(socket,
+    () => {
+      io.to(lobby).emit('user leave', `${name} left the lobby.`);
+    });
+
+  // Actually leave the room
+  leaveAllRooms(socket);
+}
 
 
+
+// Initial set up for the server to handle socket connections
 const socketSetup = (app) => {
   const server = http.createServer(app);
   io = new Server(server);
@@ -29,15 +100,14 @@ const socketSetup = (app) => {
   io.on('connection', (socket) => {
     console.log('a user connected');
 
-    /* Here we automatically put all new users into the general room. */
-    socket.join('general');
-
     socket.on('disconnect', () => {
       console.log('a user disconnected');
+      handleLeaveLobby(socket);
     });
 
-    socket.on('chat message', (msg) => handleChatMessage(socket, msg));
-    socket.on('room change', (room) => handleRoomChange(socket, room));
+    socket.on('create lobby', (data) => handleCreateLobby(socket, data));
+    socket.on('join lobby', (data) => handleJoinLobby(socket, data));
+    socket.on('leave lobby', () => handleLeaveLobby(socket));
   });
 
   return server;
