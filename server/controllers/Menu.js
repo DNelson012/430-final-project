0983582628fd50333b53ade982b485fc6031ecd6
Dan Nelson (RIT Student) <dgn8758@rit.edu>
2023-12-08 13:51:36 -0500

    Tried to get ACAO working again
    Doesn't work on a lab computer the way it worked on my own PC

--- server/controllers/Menu.js
+++ server/controllers/Menu.js
@@ -2,10 +2,10 @@ const menuPage = async (req, res) => {
   // This is probably not the best workaround for CSP blocking links
   res.header(
     'Content-Security-Policy',
-    'img-src * https://cdn.donmai.us/ https://cdn.discordapp.com/ https://pbs.twimg.com/',
+    'img-src * https://cdn.discordapp.com/ https://pbs.twimg.com/ https://www.google.com',
   );
   res.header('Access-Control-Allow-Origin', '*');
-
+            
   res.render('menu');
 };
 
